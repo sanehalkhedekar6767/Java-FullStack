8d172d3be66f73497c0943f2ad2a5d432c5ec8bc
sanehalkhedekar6767 <sanehalkhedekar6767@gmail.com>
2024-09-02 11:03:57 +0530

    Program to print list of numbers and  it's reverse order
    Program to print list of numbers and  it's reverse order

---
+++ ReverseOrderList.java
@@ -0,0 +1,14 @@
+//List of numbers and its reverse order
+public class ReverseOrderList {
+	public static void main(String[] args) {
+		
+		for(int i=1; i<=5; i++) {
+			System.out.print(i);
+		}
+		System.out.println("");
+		for(int i=5; i>=1; i--) {
+			System.out.print(i);
+		}
+	}
+
+}
