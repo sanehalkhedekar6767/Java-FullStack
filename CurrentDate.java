d0f1a7f340cb4bdd405b9e950f637ffbf37735a4
sanehalkhedekar6767 <sanehalkhedekar6767@gmail.com>
2024-09-02 11:07:12 +0530

    Program to display current date and time
    Program to display current date and time

---
+++ CurrentDate.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class CurrentDate {
+
+	public static void main(String[] args) {
+		Date Current_date = new Date();
+		System.out.println("Today's Date & Time: " +Current_date);		
+	}
+
+}
